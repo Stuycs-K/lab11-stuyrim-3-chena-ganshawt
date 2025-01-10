918479440d587b205305ee84189029402f6a3bc0
Ch1m3ras <awesomecool237@gmail.com>
2025-01-10 09:49:12 -0500

    Added Boss.java with some placeholder values, an empty file Necromaner.java for the next class, and fixed an error where getBuff would have a void return value

---
+++ Boss.java
@@ -0,0 +1,40 @@
+public class Boss extends Adventurer{
+  //ALL PLACEHOLDER VALUES CHANGE IN THE FUTURE
+
+  public Boss(){
+    super();
+  }
+
+  public String getSpecialName(){
+    return null;
+  }
+
+  public int getSpecial(){
+    return 0;
+  }
+
+  public int getSpecialMax(){
+    return 0;
+  }
+
+  public void setSpecial(int n){
+    //Stuff will go here.
+  }
+
+  public String attack(Adventurer other){
+    return null;
+  }
+
+  public String support(Adventurer other){
+    return null;
+  }
+
+  public String support(){
+    return null;
+  }
+
+  public String specialAttack(Adventurer other){
+    return null;
+  }
+
+}
