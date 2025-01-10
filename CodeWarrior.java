c8956dfa0a014048b5ff55110f94eb65c2ed9cdb
tganshaw <tudorganshaw@gmail.com>
2025-01-09 21:17:07 -0500

    Added very basic functionality for the Paladin class.
    Paladin right now is functionally just CodeWarrior but slightly changed. All I added was changing variable names, constructors, and flavor text to make sense. Will be changing everything else in the future.

--- CodeWarrior.java
+++ CodeWarrior.java
@@ -1,18 +1,16 @@
 public class CodeWarrior extends Adventurer{
-  int caffeine, caffeineMax;
-  String preferredLanguage;
+  int mana, manaMax;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
-  public CodeWarrior(String name, int hp, String language){
+  public CodeWarrior(String name, int hp){
     super(name,hp);
-    caffeineMax = 12;
-    caffeine = caffeineMax/2;
-    preferredLanguage = language;
+    manaMax = 12;
+    mana = manaMax/2;
   }
 
   public CodeWarrior(String name, int hp){
-    this(name,hp,"c++");
+    this(name,hp);
   }
 
   public CodeWarrior(String name){
@@ -20,40 +18,42 @@ public class CodeWarrior extends Adventurer{
   }
 
   public CodeWarrior(){
-    this("Carmack");
+    this("Bardon the Strong");
   }
 
   /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
-    return "caffeine";
+    return "mana";
   }
 
   public int getSpecial(){
-    return caffeine;
+    return mana;
   }
 
   public void setSpecial(int n){
-    caffeine = n;
+    mana = n;
   }
 
   public int getSpecialMax(){
-    return caffeineMax;
+    return manaMax;
   }
 
-  /*Deal 2-7 damage to opponent, restores 2 caffeine*/
+  /*Deal 2-7 damage to opponent, restores 2 mana*/
   public String attack(Adventurer other){
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
-    return this + " attacked "+ other + " and dealt "+ damage +
-    " points of damage. They then take a sip of their coffee.";
+    return this + " struck "+ other + " and dealt "+ damage +
+    " damage in the name of the kingdom.";
   }
 
-  /*Deal 3-12 damage to opponent, only if caffeine is high enough.
-  *Reduces caffeine by 8.
+  /*Deal 3-12 damage to opponent, only if mana is high enough.
+  *Reduces mana by 8.
   */
   public String specialAttack(Adventurer other){
-    if(getSpecial() >= 8){
+
+    //This becomes a revive. Don't ask how.
+  /*  if(getSpecial() >= 8){
       setSpecial(getSpecial()-8);
       int damage = (int)(Math.random()*5+Math.random()*5)+3;
       other.applyDamage(damage);
@@ -61,20 +61,19 @@ public class CodeWarrior extends Adventurer{
       " skills to hack the matrix. "+
       " This glitched out "+other+" dealing "+ damage +" points of damage.";
     }else{
-      return "Not enough caffeine to use the ultimate code. Instead "+attack(other);
+      return "Not enough mana to use the ultimate code. Instead "+attack(other);
     }
-
+*/
   }
   /*Restores 5 special to other*/
   public String support(Adventurer other){
-    return "Gives a coffee to "+other+" and restores "
+    return "Used a prayer on " + other +" and restores "
     + other.restoreSpecial(5)+" "+other.getSpecialName();
   }
   /*Restores 6 special and 1 hp to self.*/
   public String support(){
     int hp = 1;
     setHP(getHP()+hp);
-    return this+" drinks a coffee to restores "+restoreSpecial(6)+" "
-    + getSpecialName()+ " and "+hp+" HP";
+    return this+" thinks about their noble caused and restores " + hp "HP";
   }
 }
